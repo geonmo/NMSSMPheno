af8ec5925f56d397a28510423ebee84fa13aab9d
Robin Aggleton <robin.aggleton@cern.ch>
2016-02-09 11:54:45 +0000

    Refactor run_ma to move execution part to own function.
    Also update docstrings, move from call() to check_call().

--- MadAnalysis/run_ma.py
+++ MadAnalysis/run_ma.py
@@ -2,7 +2,8 @@
 """
 Run MadAnalysis locally.
 
-Takes care of untarring input files, etc.
+Takes care of untarring input files, etc. Requires an input JSON file specifying
+sample name and location.
 """
 
 
@@ -11,7 +12,7 @@ import sys
 import argparse
 import json
 import logging
-from subprocess import call
+from subprocess import check_call
 
 
 logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
@@ -52,45 +53,80 @@ def run_ma(in_args=sys.argv[1:]):
     # Do some checks
     # ------------------------------------------------------------------------
     if not os.path.isfile(args.samples):
-        raise RuntimeError('JSON samples file does not exist')
-    if not os.path.isfile(args.exe):
-        raise RuntimeError('MadAnalysis exe does not exist')
+        raise OSError('JSON samples file does not exist')
+    if not os.path.isfile(os.path.realpath(os.path.realpath(args.exe))):
+        raise OSError('MadAnalysis exe does not exist')
+    if not os.path.basename(args.exe).startswith("MadAnalysis5Job"):
+        raise RuntimeError('You need the MadAnalysis5Job exe')
 
     # Interpret samples JSON
     # ------------------------------------------------------------------------
     # create a filelist for each set of samples.
     with open(args.samples) as jfile:
         sample_dict = json.load(jfile)
-    log.debug('Sample dictionary: %s' % sample_dict)
+    log.debug('Sample dictionary: %s', sample_dict)
 
     filelists = generate_filelists(sample_dict, os.getcwd())
 
     # Run MadAnalysis
     # ------------------------------------------------------------------------
+    if not args.dry:
+        setup_run_ma(args, filelists)
+
+    return 0
+
+
+def setup_run_ma(args, filelists):
+    """Run MadAnalysis exe over filelist for each sample.
+
+    Also runs the corresponding setup.sh script.
+
+    Parameters
+    ----------
+    args : argparse.Namespace
+        User arguments from CL
+    filelists : list[str]
+        List of files which contain sample files.
+
+    Raises
+    ------
+    OSError
+        If setup.sh script cannot be found.
+    """
+    exe_abs = os.path.realpath(os.path.realpath(args.exe))
+    setup_script = os.path.join(os.path.dirname(exe_abs), 'setup.sh')
+    if not os.path.isfile(setup_script):
+        raise OSError('No setup script as %s' % setup_script)
+    setup_cmd = 'source %s;' % setup_script
     # we make a temporary inner directory to run from since the output from
     # MadAnalysis will be put in $PWD/../Output/
-    if not args.dry:
-        exe_abs = os.path.abspath(args.exe)
-        tmp_dir = 'run_dir'
-        if not os.path.isdir(tmp_dir):
-            os.makedirs(tmp_dir)
-        os.chdir(tmp_dir)
-        for flist in filelists:
-            call([exe_abs, flist])
+    tmp_dir = 'run_dir'
+    if not os.path.isdir(tmp_dir):
+        os.makedirs(tmp_dir)
+    os.chdir(tmp_dir)
+    for flist in filelists:
+        cmd = ' '.join([setup_cmd, exe_abs, flist])
+        log.debug(cmd)
+        check_call(cmd, shell=True)
 
 
 def generate_filelists(sample_dict, out_dir):
     """Generate list of files suitable for use as input to MadAnalysis.
     Each file list will be named after the sample it represents.
 
-    Returns a list of the absolute filepaths for the file lists.
-
-    sample_dict: dict
+    Parameters
+    ----------
+    sample_dict : dict
         Dict of info for channels. Key is the channel name, value is a
         dict of information about the channel.
         Channels starting with #, !, _ will be ignored.
-    out_dir: str
+    out_dir : str
         Output directory for filelists.
+
+    Returns
+    -------
+    list[str]
+        List of the absolute filepaths for the file lists.
     """
     return [create_filelist(channel, chan_dict, out_dir)
             for channel, chan_dict in sample_dict.iteritems()
@@ -99,14 +135,20 @@ def generate_filelists(sample_dict, out_dir):
 
 def create_filelist(channel, chan_dict, out_dir):
     """Create a filelist suitable for passing to MadAnalysis.
-    Returns filename of filelist.
 
-    channel: str
+    Parameters
+    ----------
+    channel : str
         Name of the channel. Used for the filelist filename.
-    chan_dict: dict
+    chan_dict : dict
         Dictionary of info corresponding to each channel
-    out_dir: str
+    out_dir : str
         Output directory for filelist.
+
+    Returns
+    -------
+    str
+        Filename of filelist.
     """
     filename = os.path.join(out_dir, channel)
 
@@ -117,7 +159,7 @@ def create_filelist(channel, chan_dict, out_dir):
                     yield os.path.join(d, f)
 
     with open(filename, 'w') as flist:
-        log.debug('Writing filelist %s' % channel)
+        log.debug('Writing filelist %s', channel)
         n_files = chan_dict['num']
         for i, f in enumerate(dir_file_iter(chan_dict['dirs'], '.root')):
             if i >= n_files and n_files > 0:
@@ -128,4 +170,4 @@ def create_filelist(channel, chan_dict, out_dir):
 
 
 if __name__ == "__main__":
-    run_ma()
+    sys.exit(run_ma())
